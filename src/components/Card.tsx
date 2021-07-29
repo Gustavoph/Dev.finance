e78b66cb8d28e6bb4c061bad3ca2d94255220ee3
Gustavo Oliveira <gustavo.gabriel@beresolution.com.br>
2021-07-29 17:48:58 -0300

    Iniciando estrutura inicial

---
+++ src/components/Card.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Box, Container, Value, Title, Paragraph } from './style';
+import ImgEntradas from '../assets/entradas.png';
+import ImgSaidas from '../assets/saidas.png';
+import ImgTotal from '../assets/total.png';
+
+const Card: React.FC = () => (
+  <Container>
+    <Box area="earnings" color="white">
+      <Title>
+        <Paragraph color="title">Entradas</Paragraph>
+        <img src={ImgEntradas} alt="" />
+      </Title>
+      <Value color="title">R$ 1.259,00</Value>    </Box>
+    <Box area="spending" color="white">
+      <Title>
+        <Paragraph color="title">Saídas</Paragraph>
+        <img src={ImgSaidas} alt="" />
+      </Title>
+      <Value color="title">R$ 1.259,00</Value>
+    </Box>
+    <Box area="total" color="green">
+      <Title>
+        <Paragraph>Total</Paragraph>
+        <img src={ImgTotal} alt="" />
+      </Title>
+      <Value>R$ 1.259,00</Value>
+    </Box>
+  </Container>
+);
+
+export default Card;
---
+++ src/components/style.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+
+interface BoxProps {
+  area: string;
+  color?: string | 'white';
+}
+
+interface ColorProps {
+  color?: string | 'white';
+}
+
+export const Container = styled.div`
+  width: 100%;
+  margin-top: -10rem;
+  height: 13.6rem;
+  display: grid;
+  grid-template-columns: 1fr 32% 1fr;
+  grid-template-rows: 1fr;
+  gap: 3%;
+  grid-template-areas: "earnings spending total";
+  justify-items: space-between;
+`;
+
+
+export const Box = styled.div<BoxProps>`
+  width: 100%;
+  padding: 3rem;
+  height: 13.6rem;
+  border-radius: 5px;
+  grid-area: ${(props) => props.area};
+  background-color: ${(props) => props.color === 'green' ? '#49AA26' : '#FFFFFF' };
+`;
+
+export const Value = styled.h1<ColorProps>`
+  font-size: 3.6rem;
+  font-weight: 400;
+  color: ${(props) => props.color === 'title' ? '#363F5F' : '#FFFFFF' };
+`;
+
+export const Title = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Paragraph = styled.p<ColorProps>`
+  font-size: 1.6rem;
+  color: ${(props) => props.color === 'title' ? '#363F5F' : '#FFFFFF' };
+`;
